8371aba53b88d383195dff9f39a6a71315fade72
Minwoo Jhung <minwoorich@naver.com>
2023-08-16 12:15:15 +0900

    백준 실버2 6603 로또

---
+++ src/minwoo/Memo.java
@@ -0,0 +1,20 @@
+package minwoo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Memo {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int N;
+    static int M;
+    static List<Integer> result;
+
+    public static void main(String[] args) throws IOException {
+        String test = br.readLine();
+        System.out.println(test);
+    }
+}
