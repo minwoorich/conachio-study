76dc6e86336f472d01924ae88ea0c668304bad32
Minwoo Jhung <minwoorich@naver.com>
2023-08-16 11:11:35 +0900

    algorithm -> conachio-stduy 파일 이동(갤럭시북)

--- src/minwoo/Main.java
+++
@@ -1,7 +0,0 @@
-package minwoo;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/minwoo/basic01/bruteforce/n_and_m/N과M_2_1560.java
@@ -0,0 +1,45 @@
+package minwoo.basic01.bruteforce.n_and_m;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class N과M_2_1560 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static boolean[] visit;
+    static int N;
+    static int M;
+    static StringBuilder output;
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        N = Integer.parseInt(input[0]);
+        M = Integer.parseInt(input[1]);
+        output = new StringBuilder("");
+        visit = new boolean[N+1];
+
+        // 처음에는 시작 값을 넘겨줌
+        dfs(1);
+    }
+
+    public static void dfs(int last) {
+        if (output.length() == M) {
+            String result = Arrays.stream(output.toString().split(""))
+                    .collect(Collectors.joining(" "));
+            System.out.println(result);
+            return;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            // 방문한 곳 금지(중복 회피) && 이전 값보다 크거나 같아야함(오름차순)
+            if (!visit[i] && last <= i) {
+                output.append(i);
+                visit[i] = true;
+                dfs(i);
+                visit[i] = false;
+                output.deleteCharAt(output.length()-1);
+            }
+        }
+    }
+}
