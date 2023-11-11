266cf532d918f0dc85e125e89d68713d76de180c
Minwoo Jhung <minwoorich@naver.com>
2023-11-11 19:51:51 +0900

    백준 골드4 17140 이차원배열과연산

--- src/minwoo/Memo.java
+++ src/minwoo/Memo.java
@@ -1,33 +1,78 @@
 package minwoo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.InputStreamReader;
+import java.util.*;
+import java.util.stream.Collectors;
 
+class MyNum implements Comparable<MyNum>{
+    int val;
+    int freq;
 
-public class Memo {
-    static List<Integer> output = new LinkedList<>();
-    static int[] list = {0, 2, 4, 5, 9, 5, 61};
-    public static void main(String[] args) throws IOException {
-        dfs(0,list[0]);
+    public MyNum(int val, int freq) {
+        this.val = val;
+        this.freq = freq;
     }
 
-    public static void dfs(int depth, int last) {
-        if (depth == 3) {
-            for (Integer integer : output) {
-                System.out.print(integer+" ");
+    @Override
+    public int compareTo(MyNum o) {
+        if (this.freq > o.freq) {
+            return 1;
+        } else if (this.freq == o.freq) {
+            if (this.val > o.val) {
+                return 1;
+            } else if (this.val == o.val) {
+                return 0;
+            }
+            return -1;
+        }
+        return -1;
+    }
+
+    @Override
+    public String toString() {
+        return "("+val+", "+freq+")";
+    }
+}
+public class Memo {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static List<List<Integer>> list = new LinkedList<>();
+    public static void main(String[] args) throws IOException {
+        List<Integer> newList = new ArrayList<>();
+        newList.add(1);
+        newList.add(0);
+        newList.add(2);
+        newList.add(2);
+        newList.add(4);
+
+        List<MyNum> myNums = convertToMyNumList(newList);
+        myNums.forEach(System.out::println);
+
+    }
+    public static List<MyNum> convertToMyNumList(List<Integer> intList) {
+        List<MyNum> myList = new ArrayList<>();
+        intList = intList.stream().filter(i->i!=0).collect(Collectors.toList());
+        int max = intList.stream().max(Integer::compareTo).get();
+        int[] freq = new int[100];
+        for (int num : intList) {
+            freq[num]++;
+        }
+        for (int i = 1; i<=max; i++) {
+            if (freq[i] != 0) {
+                myList.add(new MyNum(i, freq[i]));
+            }
+        }
+        return myList;
+    }
+    public static void print() {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).size(); j++) {
+                System.out.print(list.get(i).get(j)+" ");
             }
             System.out.println();
         }
-
-        for (int i = 0; i < list.length; i++) {
-            if (!output.contains(list[i]) &&
-                    list[i] >= last) {
-                output.add(list[i]);
-                dfs(depth + 1, list[i]);
-                output.remove(output.size() - 1);
-            }
-        }
+        System.out.println("===================");
     }
 
 }
