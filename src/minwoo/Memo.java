f1bd212ce622f8ba53cff65bb7f6a269465f6cc9
Jhung Minwoo <minwoorich96@gmail.com>
2023-11-11 23:04:54 +0900

    메모

--- src/minwoo/Memo.java
+++ src/minwoo/Memo.java
@@ -4,22 +4,23 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 
 public class Memo {
+    static List<Integer> output = new ArrayList<>();
     public static void main(String[] args) throws IOException {
-        List<Integer> list = new LinkedList<>();
+        output.add(2);
+        output.add(4);
+        output.add(6);
+        output.add(8);
 
-        list.add(1);
-        list.add(2);
-        list.add(3);
-        System.out.println(list.size());
-        list.remove(list.size() - 1);
-        System.out.println(list.size());
-        list.add(4);
-        System.out.println(list.size());
+        output = output.stream().filter(i -> !(i>6)).collect(Collectors.toList());
+        for (Integer integer : output) {
+            System.out.println(integer);
+        }
     }
 }
 
